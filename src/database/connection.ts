00ef53ec4f021fe589ce04b50768c90b5a815bda
maheshbasnet089 <basnetmanish089@gmail.com>
2024-10-18 21:19:47 +0545

    db creation

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,7 +1,9 @@
 import {Sequelize} from 'sequelize-typescript'
 import { envConfig } from '../config/config'
 
-const sequelize = new Sequelize(envConfig.connectionString as string)
+const sequelize = new Sequelize(envConfig.connectionString as string,{
+    models : [__dirname + '/models']
+})
 
 try {
     sequelize.authenticate()
