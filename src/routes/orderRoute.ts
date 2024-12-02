197417c525d45775ba48ca5d0cb3a0bfc41be092
maheshbasnet089 <basnetmanish089@gmail.com>
2024-12-02 21:04:37 +0545

    verify khalti transaction

--- src/routes/orderRoute.ts
+++ src/routes/orderRoute.ts
@@ -8,6 +8,7 @@ import errorHandler from '../services/errorHandler'
 const router:Router = express.Router()
 
 router.route("/").post(userMiddleware.isUserLoggedIn, errorHandler(orderController.createOrder))
+router.route("/verify-pidx").post(userMiddleware.isUserLoggedIn,errorHandler(orderController.verifyTransaction))
 
 
 
